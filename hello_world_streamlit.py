5cf0a87b7bcb72e80a5e6885203b5ce958d30eb4
ykdojo <yksu@csdojo.io>
2023-04-11 00:08:54 -0700

    Create hello_world_streamlit.py

---
+++ hello_world_streamlit.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Create a Streamlit app
+
+def main():
+    # Set the title of the app
+    st.title('Hello, World!')
+
+    # Display a message
+    st.write('Welcome to the Streamlit app!')
+
+if __name__ == '__main__':
+    main()
